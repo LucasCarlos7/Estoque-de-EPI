23ffa5a910fce43e685fa98a3531896e2e2ec023
Lucas Carlos <lucas@veroneze.local>
2024-01-30 14:20:59 -0300

    Create class ProductDAO and method Create

--- src/main/java/com/veroneze/program/EstoqueEPIs.java
+++ src/main/java/com/veroneze/program/EstoqueEPIs.java
@@ -1,9 +1,15 @@
 
 package com.veroneze.program;
 
+import entities.Product;
+import entities.ProductDAO;
+
 public class EstoqueEPIs {
 
     public static void main(String[] args) {
+        ProductDAO productDAO = new ProductDAO();
+        Product prod = new Product(null, "Bota PVC", "36", 10, "Bico de aço");
         
+        productDAO.Create(prod);
     }
 }
--- src/main/java/entities/ProductDAO.java
+++ src/main/java/entities/ProductDAO.java
@@ -1,5 +1,20 @@
 package entities;
 
+import jakarta.persistence.EntityManager;
+
 public class ProductDAO {
-    
+
+    public void Create(Product p) {
+        EntityManager entityManager = JPAPersistenceUseful.getEntityManager();
+        try {
+            entityManager.getTransaction().begin();
+            entityManager.persist(p);
+            entityManager.getTransaction().commit();
+        } catch (Exception e) {
+            entityManager.getTransaction().rollback();
+            throw e;
+        } finally {
+            JPAPersistenceUseful.closeEntityManager();
+        }
+    }
 }
