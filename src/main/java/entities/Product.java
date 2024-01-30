ed9fe283b8ae5d645ef84ffd2b1e99ddd3e1d580
Lucas Carlos <lucas@veroneze.local>
2024-01-30 08:13:44 -0300

    Configurando o arquivo POM.xml e PERSISTENCE.xml e craindo a classe Product

---
+++ src/main/java/entities/Product.java
@@ -0,0 +1,77 @@
+package entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Product {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
+    
+    @Column(name="number_size")
+    private String numberSize;
+    private Integer amount;
+    private String detail;
+
+    public Product() {
+    }
+
+    public Product(Integer id, String name, String numberSize, Integer amount, String detail) {
+        this.id = id;
+        this.name = name;
+        this.numberSize = numberSize;
+        this.amount = amount;
+        this.detail = detail;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getNumberSize() {
+        return numberSize;
+    }
+
+    public void setNumberSize(String numberSize) {
+        this.numberSize = numberSize;
+    }
+
+    public Integer getAmount() {
+        return amount;
+    }
+
+    public void setAmount(Integer amount) {
+        this.amount = amount;
+    }
+
+    public String getDetail() {
+        return detail;
+    }
+
+    public void setDetail(String detail) {
+        this.detail = detail;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" + "id=" + id + ", name=" + name + ", numberSize=" + numberSize + ", amount=" + amount + ", detail=" + detail + '}';
+    }
+}
