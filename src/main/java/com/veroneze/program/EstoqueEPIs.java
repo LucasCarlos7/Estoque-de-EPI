a421a91c2dfbfa36d0167dda16425ec75606d0b7
Lucas Carlos <lucas@veroneze.local>
2024-01-30 14:04:15 -0300

    Create class JPAPersistenceUseful

--- src/main/java/com/veroneze/program/EstoqueEPIs.java
+++ src/main/java/com/veroneze/program/EstoqueEPIs.java
@@ -1,21 +1,9 @@
 
 package com.veroneze.program;
 
-import entities.Product;
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.EntityManagerFactory;
-import jakarta.persistence.Persistence;
-
 public class EstoqueEPIs {
 
     public static void main(String[] args) {
         
-        EntityManagerFactory emf = Persistence.createEntityManagerFactory("VERONEZE_EPI_DB");
-        EntityManager manager = emf.createEntityManager();
-        
-        Product p = new Product(null, "teste", "P", 10, "Teste");
-        manager.getTransaction().begin();
-        manager.persist(p);
-        manager.getTransaction().commit();
     }
 }
---
+++ src/main/java/entities/JPAPersistenceUseful.java
@@ -0,0 +1,37 @@
+package entities;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
+public class JPAPersistenceUseful {
+
+    /* constante para centraizar o nome da unidade de persistência
+       se o nome mudar, precisaremos alterar em um só lugar */
+    private static final String PERSISTENCE_UNIT = "VERONEZE_EPI_DB";
+
+    private static EntityManager entityManager;
+    private static EntityManagerFactory entityManagerFactory;
+
+    // cria a entidade se estiver nula e retorna
+    public static EntityManager getEntityManager() {
+        if (entityManagerFactory == null || !entityManagerFactory.isOpen()) {
+            entityManagerFactory = Persistence.createEntityManagerFactory(PERSISTENCE_UNIT);
+        }
+
+        //cria se em nulo ou se o entity manager foi fechado
+        if (entityManager == null || !entityManager.isOpen()) {
+            entityManager = entityManagerFactory.createEntityManager();
+        }
+
+        return entityManager;
+    }
+
+    //fecha o EntityManager e o EntityManagerFactory
+    public static void closeEntityManager() {
+        if (entityManager.isOpen() && entityManager != null) {
+            entityManager.close();
+            entityManagerFactory.close();
+        }
+    }
+}
